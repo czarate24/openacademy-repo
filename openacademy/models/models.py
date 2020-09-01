568e6a0f3ca4d4a1e740956517e61cf3c9d23235
Carlos Zarate <czarate@pyansa.com.mx>
2020-08-31 19:17:30 -0600

    [REF] openacademy: Add inherit matter

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'Clase o modulo para definir cursos'
-     
+         
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
     responsible_id = fields.Many2one(
@@ -16,12 +16,14 @@ class Course(models.Model):
 
 class Session(models.Model):
     _name = 'openacademy.session'
+    _description = 'Clase o modulo para definir sesiones'
 
     name = fields.Char(required=True)
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string='Instructor')
+    instructor_id = fields.Many2one('res.partner', string='Instructor', 
+                                     domain=['|',('instructor','=', True), ('category_id.name','ilike','Teacher''')])
     course_id = fields.Many2one('openacademy.course', ondelete='cascade',
                                 string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")                            
